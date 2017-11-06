1bc0d5fe11c9f385d88c59d893153fe8c3a6db67
Midani Rachdi <midani.rachdi@esprit.tn>
2017-11-06 15:47:20 +0100

    application insights removed from gitignore , added webserviceConsuming (90%)

---
+++ RefugeeCamp.Web/Controllers/WebServiceConsumeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using RefugeeCamp.Domain.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace RefugeeCamp.Web.Controllers
+{
+   
+    public class WebServiceConsumeController : Controller
+    {
+        string Baseurl = "http://localhost:18080/";
+        // GET: WebServiceConsume
+        public async Task<ActionResult> Index()
+        {
+            List<user> EmpInfo = new List<user>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("refugeesCamp-web/api/users");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    EmpInfo = JsonConvert.DeserializeObject<List<user>>(EmpResponse);
+
+                }
+                //returning the employee list to view  
+                return View(EmpInfo);
+            }
+        }
+    }
+}
