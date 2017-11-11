388126bc32b757d7abc5e195f706c82a56d634b8
Mohamad khreibi <mohamad.khreibi@esprit.tn>
2017-11-11 17:10:23 +0100

    add security layer

---
+++ RefugeeCamp.Web/Security/CustomAuthorizeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RefugeeCamp.Web.Security
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (string.IsNullOrEmpty(SessionPersister.Token))
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Account", action = "Index" }));
+            else
+            {
+                CustomPrincipal mp = new CustomPrincipal(SessionPersister.User);
+                if (!mp.IsInRole(Roles))
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "AccessDenied", action = "Index" }));
+            }
+        }
+
+    }
+}
---
+++ RefugeeCamp.Web/Security/CustomPrincipal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using RefugeeCamp.Domain.Models;
+
+namespace RefugeeCamp.Web.Security
+{
+    public class CustomPrincipal : IPrincipal
+    {
+
+        private user User;
+
+
+        public CustomPrincipal(user user)
+        {
+            this.User = user;
+            this.Identity = new GenericIdentity(user.email);
+        }
+
+        public IIdentity Identity { get; set; }
+
+        public bool IsInRole(string role)
+        {
+            var roles = role.Split(new char[] { ',' });
+            return roles.Any(r => this.User.GetType().BaseType.Name.Equals(r));
+        }
+    }
+}
