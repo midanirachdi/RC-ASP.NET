55bfa1b507388f681bfad9977b124ecc9d6a39e3
Mohamad khreibi <mohamad.khreibi@esprit.tn>
2017-11-11 17:15:52 +0100

    login service

--- RefugeeCamp.Service/GestionUser.cs
+++ RefugeeCamp.Service/GestionUser.cs
@@ -1,5 +1,12 @@
-﻿using RefugeeCamp.Data.Infrastructures;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RefugeeCamp.Data.Infrastructures;
 using RefugeeCamp.Domain.Models;
+using RefugeeCamp.Service.utile;
 using Service.Pattern;
 
 namespace RefugeeCamp.Service
@@ -13,9 +20,40 @@ namespace RefugeeCamp.Service
         {
         }
 
-        //public IEnumerable<joboffer> ListerProductByCategoryIn2008(string name)
-        //{
-        //    return utw.GetRepository<Product>().FindByCondition(x=>x.MyCategory.NameCategory==name).Where(x=>x.DateProd.Year==2008);
-        //}
+
+
+        public async Task<string> callLogin(string username, string password)
+        {
+            string token = null;
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("http://localhost:18080/refugeesCamp-web/api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Base64Utilities.EncodeTo64(username + ":" + password));
+                HttpResponseMessage response = await client.GetAsync("home/login").ConfigureAwait(false);
+                if (response.IsSuccessStatusCode)
+                {
+                    token = response.Headers.GetValues("Authorization").SingleOrDefault();
+
+                }
+
+            }
+            return token;
+        }
+
+
+        public user GetUserByToken(string token)
+        {
+            token = token.Substring(7);
+            string[] jwtStrings = token.Split('.');
+            dynamic payload = JsonConvert.DeserializeObject<dynamic>(Base64Utilities.Decode64ToString(jwtStrings[1]));
+            int i;
+
+            Int32.TryParse(payload.id.ToString(), out i);
+            return FindById(i);
+        }
+
     }
 }
---
+++ RefugeeCamp.Service/Utile/Base64Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefugeeCamp.Service.utile
+{
+    public class Base64Utilities
+    {
+        static public string EncodeTo64(string toEncode)
+        {
+            byte[] toEncodeAsBytes
+                = System.Text.UTF8Encoding.UTF8.GetBytes(toEncode);
+            string returnValue
+                = System.Convert.ToBase64String(toEncodeAsBytes);
+            return returnValue;
+        }
+
+        static public string Decode64ToString(string toDecode)
+        {
+            string base64Decoded;
+            byte[] data = System.Convert.FromBase64String(toDecode);
+            base64Decoded = System.Text.UTF8Encoding.UTF8.GetString(data);
+            return base64Decoded;
+        }
+    }
+}
