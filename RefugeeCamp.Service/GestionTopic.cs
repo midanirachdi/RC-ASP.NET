f6d1f180e62484cee1662222baeb0deb4cd30491
Mohamad khreibi <mohamad.khreibi@esprit.tn>
2017-11-13 15:39:17 +0100

    list view topic

---
+++ RefugeeCamp.Service/GestionTopic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RefugeeCamp.Data.Infrastructures;
+using RefugeeCamp.Domain.Models;
+using Service.Pattern;
+
+namespace RefugeeCamp.Service
+{
+    public class GestionTopic : Service<topic>
+    {
+        static DatabaseFactory dbFactory = new DatabaseFactory();
+        static UnitOfWork utw = new UnitOfWork(dbFactory);
+        public GestionTopic() : base(utw)
+        {
+        }
+
+
+        public List<topic> getAllTopics()
+        {
+            return FindByCondition().ToList();
+        }
+    }
+}
