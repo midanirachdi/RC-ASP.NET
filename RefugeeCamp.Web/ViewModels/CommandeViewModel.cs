9f364b66a3b973550d12b2645e69d05465049cc0
Lukenjevi <salimbhassine@gmail.com>
2017-11-14 21:48:16 +0100

    adding ModelViews

---
+++ RefugeeCamp.Web/ViewModels/CommandeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RefugeeCamp.Web.ViewModels
+{
+    public class CommandeViewModel
+    {
+        public string address { get; set; }
+        public string country { get; set; }
+        public int qteOfProduct { get; set; }
+
+        public virtual string ProviderID { get; set; }
+
+        public virtual SelectList ProviderList { get; set; }
+    }
+}
---
+++ RefugeeCamp.Web/ViewModels/ProviderListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RefugeeCamp.Web.ViewModels
+{
+    public class ProviderListViewModel
+    {
+        public int ProviderId { set; get; }
+        public string Nom { set; get; }
+
+    }
+}
