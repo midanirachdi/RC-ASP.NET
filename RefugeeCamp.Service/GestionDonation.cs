5e8004be6e817b693bb917dc54c48e57f4bf55fb
Khalil Fakhfekh <khalil.fakhfekh@esprit.tn>
2017-11-15 04:40:44 +0100

    donation 20%

---
+++ RefugeeCamp.Service/GestionDonation.cs
@@ -0,0 +1,22 @@
+﻿using RefugeeCamp.Data.Infrastructures;
+using RefugeeCamp.Domain.Models;
+using Service.Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefugeeCamp.Service
+{
+    public class GestionDonation : Service<donation> , IGestionDonation
+    {
+        static DatabaseFactory dbFactory = new DatabaseFactory();
+        static UnitOfWork utw = new UnitOfWork(dbFactory);
+
+        public GestionDonation() : base(utw)
+        {
+
+        }
+    }
+}
