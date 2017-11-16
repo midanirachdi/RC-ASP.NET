196ef2530c9e1cc3993dac14a4e39a9ce361da72
Lukenjevi <salimbhassine@gmail.com>
2017-11-16 03:04:33 +0100

    Adding Order functionalities

--- RefugeeCamp.Web/Controllers/CommandeController.cs
+++ RefugeeCamp.Web/Controllers/CommandeController.cs
@@ -15,11 +15,12 @@ namespace RefugeeCamp.Web.Controllers
     public class CommandeController : Controller
     {
         private GestionCommande gCommandes = null;
+        IQueryable<commande> ordersList = null;
         //private GestionStock gStock = null;
         public CommandeController()
         {
             gCommandes = new GestionCommande();
-
+            ordersList = gCommandes.QueryObjectGraph("commande");
         }
 
         // GET: Commande
@@ -45,12 +46,22 @@ namespace RefugeeCamp.Web.Controllers
         
             [HttpPost]
             [Route("/{type}")]
-            
             public ActionResult Create(CommandeViewModel cmdVM, string type)
             {
                 user currentUser = SessionPersister.User;
                 var StockCtrl = new GestionStock();
-            //stock currentStock =  StockCtrl.FindByCondition(p=>p.stockType.Equals(type)).First();
+            commande cmd = new commande();
+            cmd.address = cmdVM.Commande.address;
+            cmd.country = cmdVM.Commande.country;
+            cmd.qteOfProduct = cmdVM.Commande.qteOfProduct;
+            cmd.totalPrice = 0;
+            cmd.status = 0;
+            cmd.ordered = DateTime.Now;//.ToString("yyyy-MM-dd HH:mm:ss");
+            cmd.Admin = currentUser.id;
+            cmd.Provider = cmdVM.PostedProviderId;
+            cmd.Stock = 1;
+
+            /*//stock currentStock =  StockCtrl.FindByCondition(p=>p.stockType.Equals(type)).First();
             commande cmd = new commande();//cmdVM.Commande;
             provider prv = new provider();
             GestionProvider gProvider = new GestionProvider();
@@ -61,18 +72,86 @@ namespace RefugeeCamp.Web.Controllers
             //cmd.provider1 = prv;
 
             cmd.totalPrice=0;
-            cmd.status=0;
-                if (ModelState.IsValid)
+            cmd.status=0;*/
+            if (ModelState.IsValid)
                 {
                     gCommandes.Create(cmd);
                     gCommandes.Commit();
-
-                   
                     return RedirectToAction("Index");
                 }
                 else return View(cmd);
                
             }
+
+        public ActionResult List()
+        {
+            return View(this.getOrdersViewModel());
+            
+        }
+        
+        public ActionResult ChangeStatusToShipped(int id)
+        {
+            commande cmd = gCommandes.FindById(id);
+            cmd.status = 1;
+            cmd.shipped = DateTime.Now;
+            gCommandes.Update(cmd);
+            gCommandes.Commit();
+            return RedirectToAction("List");
+
+        }
+
+        public ActionResult Details(int id)
+        {
+            OrdersViewModel ordv = getOrdersViewModel().Find(e => e.Cmd.id == id);
+            
+            return View(ordv);
+        }
+        public ActionResult Update(int id)
+        {
+            commande cmdCurrent = gCommandes.FindById(id);
+            return View(cmdCurrent);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(int id, FormCollection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                commande prov = gCommandes.FindById(id);
+                prov.address = collection["address"];
+                prov.country = collection["country"];
+                prov.qteOfProduct = int.Parse(collection["qteOfProduct"]);
+                
+                gCommandes.Update(prov);
+                gCommandes.Commit();
+                return RedirectToAction("List");
+            }
+            else
+                return View();
+        }
+
+        public List<OrdersViewModel> getOrdersViewModel()
+        {
+            var list = ordersList.Select(s => new { s.id, s.address, s.country, s.ordered, s.qteOfProduct, s.shipped, s.status, s.totalPrice, s.Admin, s.Stock, s.Provider }).ToList();
+
+            List<OrdersViewModel> orders = new List<OrdersViewModel>();
+            foreach (var s in list)
+            {
+                //Console.WriteLine(s.Admin1.firstName);
+                GestionUser gu = new GestionUser();
+                GestionStock gs = new GestionStock();
+                GestionProvider gp = new GestionProvider();
+
+                commande cm = new commande { id = s.id, Admin = s.Admin, Stock = s.Stock, Provider = s.Provider, address = s.address, country = s.country, ordered = s.ordered, qteOfProduct = s.qteOfProduct, shipped = s.shipped, status = s.status, totalPrice = s.totalPrice };
+
+                user a = gu.FindById(cm.Admin.GetValueOrDefault());
+                stock st = gs.FindById(cm.Stock.GetValueOrDefault());
+                provider p = gp.FindById(cm.Provider.GetValueOrDefault());
+                orders.Add(new OrdersViewModel { Cmd = cm, AdminName = a.FullName, ProviderName = p.nom, StockType = st.stockType });
+            }
+            return orders;
+        }
 
     }
 }
