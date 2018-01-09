52880c1f84737d1d46848823cedcc4ed9f36662a
Lukenjevi <salimbhassine@gmail.com>
2018-01-09 15:30:17 +0100

    Adding orders and commands

---
+++ RefugeeCamp.Web/ViewModels/StatViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RefugeeCamp.Web.ViewModels
+{
+    public class StatViewModel
+    {
+        public int pending { get; set; }
+        public int recived { get; set; }
+    }
+}
